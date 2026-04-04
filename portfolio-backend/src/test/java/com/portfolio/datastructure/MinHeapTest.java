b87740413f76d161c70e2d5fea75ba91697d3e37
Yoan Avramov <yoanavramov0304@gmail.com>
2026-04-04 12:39:30 +0300

    add MinHeapTest topK algorithm

--- portfolio-backend/src/test/java/com/portfolio/datastructure/MinHeapTest.java
+++ portfolio-backend/src/test/java/com/portfolio/datastructure/MinHeapTest.java
@@ -22,3 +22,27 @@ class MinHeapTest {
         MinHeap<Integer> heap = new MinHeap<Integer>(Comparator.naturalOrder());
         heap.insert(7); heap.insert(2);
         assertEquals(2, heap.peekMin());
+        assertEquals(2, heap.peekMin());
+        assertEquals(2, heap.size());
+    }
+
+    @Test
+    void topKAlgorithmSelectsLargestK() {
+        MinHeap<Integer> heap = new MinHeap<Integer>(Comparator.naturalOrder());
+        int k = 3;
+        for (int v : new int[]{4, 1, 7, 9, 2, 6}) {
+            heap.insert(v);
+            if (heap.size() > k) heap.extractMin();
+        }
+        // heap should contain the 3 largest: 6, 7, 9
+        assertEquals(3, heap.size());
+        assertEquals(6, heap.extractMin());
+        assertEquals(7, heap.extractMin());
+        assertEquals(9, heap.extractMin());
+    }
+
+    @Test
+    void reversedComparatorGivesMaxHeap() {
+        MinHeap<Integer> heap = new MinHeap<Integer>(Comparator.reverseOrder());
+        heap.insert(3); heap.insert(8); heap.insert(1);
+        assertEquals(8, heap.extractMin());
